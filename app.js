27d2933aa1e9638eb0fa1dabfe1ba69c7af1a9ab
unknown <j.poliveira90@outlook.com>
2023-01-23 11:37:19 -0500

    Completed persons display

--- app.js
+++ app.js
@@ -140,6 +140,15 @@ function displayPeople(people) {
 function displayPerson(person) {
     let personInfo = `First Name: ${person.firstName}\n`;
     personInfo += `Last Name: ${person.lastName}\n`;
+    personInfo += `Gender: ${person.gender}\n`;
+    personInfo += `DOB: ${person.dob}\n`;
+    personInfo += `Height: ${person.height}\n`;
+    personInfo += `Weight: ${person.weight}\n`;
+    personInfo += `Eye Color: ${person.eyeColor}\n`;
+    personInfo += `Occupation: ${person.occupation}\n`;
+
+    // let personInfo =JSON.stringify(person, null, " ")
+
     //! TODO #1a: finish getting the rest of the information to display //////////////////////////////////////////
     alert(personInfo);
 }
